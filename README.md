0a6e92bb2153e0e37fd9e8746e08d47569ca1a87
Fredrik Liljedahl <io.qrdr@gmail.com>
2025-04-06 13:11:48 +0200

    fix: remove image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [Callum](https://github.com/qmk/qmk_firmware/tree/user-keymaps-still-present/users/callum) based keymap.
 
-![Image of keymap](./keymap.png)
+![Image of keymap](https://github.com/user-attachments/assets/3b829176-bce1-4fae-a9c3-d7a18f18b351)
--- keymap.c
+++ keymap.c
@@ -33,8 +33,8 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEF] = LAYOUT_split_3x5_3_ex2(
-      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    SE_COMM, SE_DOT,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    SE_EXLM, SE_QUES, KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA,
+      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    SE_EXLM, SE_QUES, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    SE_COMM, SE_DOT,  KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA,
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    SE_ARNG, SE_ADIA, KC_SLASH,
                         LA_NUM,  LA_NAV,  KC_SPC,                    KC_LSFT, LA_SYM,  LA_FUN
     ),
